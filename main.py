45a0f162c0b7c384a35b5e419d17c330bfe53ffc
u001142 <victorgrabenko@gmail.com>
2025-04-13 23:04:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 # Телеграм-бот знайомств "Знайома"
 # Мінімально робоча структура з підтримкою анкет, лайків, чатів, преміум, рефералів та мультимовного інтерфейсу
 
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
 import logging
 import asyncio
 import sqlite3
